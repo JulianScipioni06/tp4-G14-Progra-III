e7d591005174321541a26f966f7c0402596a2f40
Joaquin Wener <joaquinwener@gmail.com>
2026-05-28 16:15:17 -0300

    Creamos el metodo GET y modificamos el archivo de rutas

--- routes/alumno.routes.js
+++ routes/alumno.routes.js
@@ -3,13 +3,17 @@ const {
   getAlumnoAll,
   getAlumnoById,
   postAlumno,
+  putAlumno,
 } = require("../controllers/alumno.controller");
 
+console.log("putalumno es: ", putAlumno);
 const rutas = Router();
 
 rutas.get("/", getAlumnoAll);
 rutas.get("/:legajo", getAlumnoById);
 
 rutas.post("/", postAlumno);
+
+rutas.put("/:legajo", putAlumno)
 
 module.exports = rutas;
